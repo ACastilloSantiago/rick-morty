a892622833c48372359b4f4935861e87a005618e
ACastilloSantiago <santiagorodrigec@gmail.com>
2023-12-27 14:58:23 -0300

    back-update

--- back/src/controllers/postFav.js
+++ back/src/controllers/postFav.js
@@ -2,12 +2,12 @@ const { Favorite } = require("../DB_connection");
 
 const postFav = async (req, res) => {
   try {
-    const { name, origin, status, image, species, gender } = req.body;
+    const { name, origin, status, image, species, gender, id } = req.body;
     if (!name || !origin || !status || !image || !species || !gender) {
       return res.status(401).send("Faltan datos");
     }
     await Favorite.findOrCreate({
-      where: { name, origin: origin.name, status, image, species, gender },
+      where: { name, origin: origin.name, status, image, species, gender, id },
     });
     const allFavs = await Favorite.findAll();
     res.status(200).send(allFavs);
